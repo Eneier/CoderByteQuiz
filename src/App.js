404776d53207c8062f9beb4ef8abdab8ff2d693b
Eneier <canadacoder94@gmail.com>
2022-10-07 11:45:55 -0400

    UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const handlerSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div className="container">
+            <div className="nav">
+                <div className="title">Quiz number #1</div>
+                <button className="btn primary">Next</button>
+            </div>
+            <div className="card">
+                <div className="item1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi dolorum
+                    ducimus ex libero magni natus repellat sit temporibus vitae?
+                </div>
+                <div className="variants">
+                    <h3>SELECT ONLY ONE</h3>
+                    <form action={handlerSubmit}>
+
+
+                        <input type="radio" id="var1" name="radio-group"/>
+                        <label htmlFor="var1">Variant 1</label>
+
+                        <input type="radio" id="var2" name="radio-group"/>
+                        <label htmlFor="var2">Variant 2</label>
+
+                        <input type="radio" id="var3" name="radio-group"/>
+                        <label htmlFor="var3">Variant 3</label>
+
+                        <input type="radio" id="var4" name="radio-group"/>
+                        <label htmlFor="var4">Variant 4</label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/store/state.js
@@ -0,0 +1,112 @@
+export const questions = [
+    {
+        "id": 1,
+        "title": "Question number 1",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 0
+    },
+    {
+        "id": 2,
+        "title": "Question number 2",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 1
+    },
+    {
+        "id": 3,
+        "title": "Question number 3",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 2
+    },
+    {
+        "id": 4,
+        "title": "Question number 4",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 3
+    },
+    {
+        "id": 5,
+        "title": "Question number 5",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 0
+    },
+    {
+        "id": 6,
+        "title": "Question number 6",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 1
+    },
+    {
+        "id": 7,
+        "title": "Question number 7",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 2
+    },
+    {
+        "id": 8,
+        "title": "Question number 8",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 3
+    },
+    {
+        "id": 9,
+        "title": "Question number 9",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 0
+    },
+    {
+        "id": 10,
+        "title": "Question number 10",
+        "variants": [
+            "variant 1",
+            "variant 2",
+            "variant 3",
+            "variant 4"
+        ],
+        "correct": 1
+    }
+]
